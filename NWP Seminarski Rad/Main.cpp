6cf23956f0a107ed7a7b3d848dcfdb54ff2adaa9
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2020-08-24 11:13:53 +0200

    Add map loading

--- NWP Seminarski Rad/Main.cpp
+++ NWP Seminarski Rad/Main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <fstream>
 #include <iostream>
 
 int main()
@@ -11,6 +12,42 @@ int main()
 	sf::Sprite player;
 	playerTexture.loadFromFile("Ash_Sprites.png", sf::IntRect(0, 0, 30, 30));
 	player.setTexture(playerTexture);
+
+	sf::Texture tileTexture;
+	sf::Sprite tile;
+	tileTexture.loadFromFile("Tile_Set.png");
+	tile.setTexture(tileTexture);
+
+	std::ifstream file("testMap.txt");
+	if (!file.is_open())
+	{
+		return NULL;
+	}
+
+	sf::Vector2i map[100][100];
+	sf::Vector2i loadCounter = sf::Vector2i(0, 0);
+
+	while (!file.eof())
+	{
+		std::string str;
+		file >> str;
+		char x = str[0];
+		char y = str[2];
+
+		map[loadCounter.x][loadCounter.y] = sf::Vector2i(x - '0', y - '0');
+
+		if (file.peek() == '\n')
+		{
+			loadCounter.x = 0;
+			loadCounter.y++;
+		}
+		else
+		{
+			loadCounter.x++;
+		}
+	}
+
+	file.close();
 
 	while (window.isOpen())
 	{
@@ -68,6 +105,17 @@ int main()
 		window.setView(view);
 
 		window.clear();
+
+		for (int h = 0; h < 10; ++h)
+		{
+			for (int w = 0; w < 10; ++w)
+			{
+				tile.setPosition(h * 30, w * 30);
+				tile.setTextureRect(sf::IntRect(map[h][w].x * 30, map[h][w].y * 30, 30, 30));
+				window.draw(tile);
+			}
+		}
+
 		window.draw(player);
 		window.display();
 	}
