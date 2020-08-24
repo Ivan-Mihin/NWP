9f486c27163e88a732f8a04f53edafae0b4488cc
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2020-08-24 09:14:15 +0200

    Add game loop

--- NWP Seminarski Rad/Main.cpp
+++ NWP Seminarski Rad/Main.cpp
@@ -1,7 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(1000, 800), "Pokemon");
 
+
+	while (window.isOpen())
+	{
+		sf::Event gameEvent;
+
+		while (window.pollEvent(gameEvent))
+		{
+			switch (gameEvent.type)
+			{
+			case gameEvent.Closed:
+			{
+				window.close();
+				break;
+			}
+
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return 0;
 }
