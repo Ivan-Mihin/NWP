036da464ca37d05d1dfd788e7063386f4dc6cc05
Ivan Mihin <56129012+Ivan-Mihin@users.noreply.github.com>
2020-09-11 14:04:50 +0200

    Update Battle class

--- NWP Seminarski Rad/Battle.cpp
+++ NWP Seminarski Rad/Battle.cpp
@@ -1,16 +1,16 @@
 #pragma once
 #include <string>
 #include "Battle.h"
-#include "Button.h"
-#include "Pokemon.h"
 
 Battle::Battle()
 {
 	texture.loadFromFile("Textures/Battle_Background.png");
 	background.setTexture(texture);
 	background.setPosition(0.0f, 0.0f);
+
+	enemy_turn = false;
 }
-	
+
 void Battle::start(Pokemon pokemon)
 {
 	sf::RenderWindow battle_window(sf::VideoMode(640, 480), "Battle", sf::Style::Titlebar);
@@ -20,58 +20,32 @@ void Battle::start(Pokemon pokemon)
 	sf::Font font;
 	font.loadFromFile("Fonts/Pokemon_GB.ttf");
 
-	sf::Text enemy_pokemon_name;
-	enemy_pokemon_name.setFont(font);
-	enemy_pokemon_name.setString(this->pokemon.getName());
-	enemy_pokemon_name.setCharacterSize(16);
-	enemy_pokemon_name.setFillColor(sf::Color::White);
-	enemy_pokemon_name.setStyle(sf::Text::Bold);
-	enemy_pokemon_name.setPosition(10.0f, 10.0f);
+	textInitialization(&enemy_pokemon_name, &font, 16, this->pokemon.getName(), sf::Color::White, 10.0f, 10.0f, false);
+	textInitialization(&enemy_pokemon_health, &font, 16, "Health: " + std::to_string(this->pokemon.hit_points), sf::Color::White, 10.0f, 30.0f, false);
 
-	sf::Text enemy_pokemon_health;
-	enemy_pokemon_health.setFont(font);
-	enemy_pokemon_health.setString("Health: " + std::to_string(this->pokemon.hit_points));
-	enemy_pokemon_health.setCharacterSize(16);
-	enemy_pokemon_health.setFillColor(sf::Color::White);
-	enemy_pokemon_health.setPosition(10.0f, 30.0f);
+	textInitialization(&player_pokemon_name, &font, 16, pokemon.getName(), sf::Color::White, 475.0f, 320.0f, true);
+	textInitialization(&player_pokemon_health, &font, 16, "Health: " + std::to_string(this->pokemon.hit_points), sf::Color::White, 475.0f, 340.0f, true);
 
-	sf::Text player_pokemon_name;
-	player_pokemon_name.setFont(font);
-	player_pokemon_name.setString(pokemon.getName());
-	player_pokemon_name.setCharacterSize(16);
-	player_pokemon_name.setFillColor(sf::Color::White);
-	player_pokemon_name.setStyle(sf::Text::Bold);
-	player_pokemon_name.setOrigin(player_pokemon_name.getLocalBounds().left + player_pokemon_name.getLocalBounds().width,
-		player_pokemon_name.getLocalBounds().top + player_pokemon_name.getLocalBounds().height);
-	player_pokemon_name.setPosition(475.0f, 400.0f);
-
-	sf::Text player_pokemon_health;
-	player_pokemon_health.setFont(font);
-	player_pokemon_health.setString("Health: " + std::to_string(this->pokemon.hit_points));
-	player_pokemon_health.setCharacterSize(16);
-	player_pokemon_health.setFillColor(sf::Color::White);
-	player_pokemon_health.setOrigin(player_pokemon_health.getLocalBounds().left + player_pokemon_health.getLocalBounds().width,
-		player_pokemon_health.getLocalBounds().top + player_pokemon_health.getLocalBounds().height);
-	player_pokemon_health.setPosition(475.0f, 420.0f);
-
-	Button move1("Textures/Battle_Button.png", 135, 50, 495, 107);
-	Button move2("Textures/Battle_Button.png", 135, 50, 495, 162);
-	Button move3("Textures/Battle_Button.png", 135, 50, 495, 217);
-	Button move4("Textures/Battle_Button.png", 135, 50, 495, 272);
-	Button run("Textures/Battle_Button.png", 135, 50, 495, 420);
+	Button move1_button("Textures/Battle_Button.png", 135, 50, 495, 107);
+	Button move2_button("Textures/Battle_Button.png", 135, 50, 495, 162);
+	Button move3_button("Textures/Battle_Button.png", 135, 50, 495, 217);
+	Button move4_button("Textures/Battle_Button.png", 135, 50, 495, 272);
+	Button run_button("Textures/Battle_Button.png", 135, 50, 495, 420);
 
 	sf::Font font2;
 	font2.loadFromFile("Fonts/arial.ttf");
 
-	move1.textInitialize(&font2, pokemon.move1.name, 15, sf::Color::Black);
-	move2.textInitialize(&font2, pokemon.move2.name, 15, sf::Color::Black);
-	move3.textInitialize(&font2, pokemon.move3.name, 15, sf::Color::Black);
-	move4.textInitialize(&font2, pokemon.move4.name, 15, sf::Color::Black);
-	run.textInitialize(&font2, "RUN", 15, sf::Color::Black);
+	move1_button.textInitialization(&font2, 15, pokemon.move1.name, sf::Color::Black);
+	move2_button.textInitialization(&font2, 15, pokemon.move2.name, sf::Color::Black);
+	move3_button.textInitialization(&font2, 15, pokemon.move3.name, sf::Color::Black);
+	move4_button.textInitialization(&font2, 15, pokemon.move4.name, sf::Color::Black);
+	run_button.textInitialization(&font2, 15, "RUN", sf::Color::Black);
 
 	pokemon.sprite.setScale(2.0f, 2.0f);
 	pokemon.sprite.setOrigin(0, pokemon.sprite.getLocalBounds().top + pokemon.sprite.getLocalBounds().height);
-	pokemon.sprite.setPosition(20, battle_window.getSize().y);
+	pokemon.sprite.setPosition(20, 400);
+
+	battle_window.clear();
 
 	while (battle_window.isOpen())
 	{
@@ -79,6 +53,13 @@ void Battle::start(Pokemon pokemon)
 
 		while (battle_window.pollEvent(battle_event))
 		{
+			if (enemy_turn)
+			{
+				enemyTurn(&battle_window, &pokemon, &player_pokemon_health, &enemy_pokemon_health);
+
+				enemy_turn = false;
+			}
+
 			switch (battle_event.type)
 			{
 			case battle_event.Closed:
@@ -86,138 +67,173 @@ void Battle::start(Pokemon pokemon)
 				battle_window.close();
 				break;
 			}
+
 			case battle_event.MouseButtonPressed:
 			{
 				sf::Vector2i mouse_position = sf::Mouse::getPosition(battle_window);
 
 				if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 				{
-					if (move1.isClicked(mouse_position))
+					if (move1_button.isClicked(mouse_position) || move2_button.isClicked(mouse_position) || move3_button.isClicked(mouse_position) || move4_button.isClicked(mouse_position))
 					{
-						this->pokemon.hit_points -= pokemon.move1.value;
-						enemy_pokemon_health.setString("Health: " + std::to_string(this->pokemon.hit_points));
+						playerTurn(move1_button, move2_button, move3_button, move4_button, mouse_position);
 
-						if (this->pokemon.hit_points <= 0)
-						{
-							return;
-						}
-
-						enemyAttack(&battle_window, &pokemon, &player_pokemon_health, &enemy_pokemon_health);
+						enemy_turn = true;
 					}
 
-					if (move2.isClicked(mouse_position))
+					if (run_button.isClicked(mouse_position))
 					{
-						pokemon.hit_points += pokemon.move2.value;
-						player_pokemon_health.setString("Health: " + std::to_string(pokemon.hit_points));
-
-						enemyAttack(&battle_window, &pokemon, &player_pokemon_health, &enemy_pokemon_health);
-					}
-
-					if (move3.isClicked(mouse_position))
-					{
-						this->pokemon.hit_points -= pokemon.move3.value;
-						enemy_pokemon_health.setString("Health: " + std::to_string(this->pokemon.hit_points));
-
-						if (this->pokemon.hit_points <= 0)
-						{
-							return;
-						}
-
-						enemyAttack(&battle_window, &pokemon, &player_pokemon_health, &enemy_pokemon_health);
-					}
-
-					if (move4.isClicked(mouse_position))
-					{
-						this->pokemon.hit_points -= pokemon.move4.value;
-						enemy_pokemon_health.setString("Health: " + std::to_string(this->pokemon.hit_points));
-
-						if (this->pokemon.hit_points <= 0)
-						{
-							return;
-						}
-
-						enemyAttack(&battle_window, &pokemon, &player_pokemon_health, &enemy_pokemon_health);
-					}
-
-					if (run.isClicked(mouse_position))
-					{
-						return;
+						battle_window.close();
 					}
 				}
 			}
 			}
 
-		battle_window.clear();
-		battle_window.draw(background);
+			battle_window.draw(background);
 
-		battle_window.draw(enemy_pokemon_name);
-		battle_window.draw(enemy_pokemon_health);
+			battle_window.draw(enemy_pokemon_name);
+			battle_window.draw(enemy_pokemon_health);
 
-		battle_window.draw(player_pokemon_name);
-		battle_window.draw(player_pokemon_health);
+			battle_window.draw(player_pokemon_name);
+			battle_window.draw(player_pokemon_health);
 
-		battle_window.draw(move1.sprite);
-		battle_window.draw(move1.text);
+			battle_window.draw(move1_button.sprite);
+			battle_window.draw(move1_button.text);
 
-		battle_window.draw(move2.sprite);
-		battle_window.draw(move2.text);
+			battle_window.draw(move2_button.sprite);
+			battle_window.draw(move2_button.text);
 
-		battle_window.draw(move3.sprite);
-		battle_window.draw(move3.text);
+			battle_window.draw(move3_button.sprite);
+			battle_window.draw(move3_button.text);
 
-		battle_window.draw(move4.sprite);
-		battle_window.draw(move4.text);
+			battle_window.draw(move4_button.sprite);
+			battle_window.draw(move4_button.text);
 
-		battle_window.draw(run.sprite);
-		battle_window.draw(run.text);
+			battle_window.draw(run_button.sprite);
+			battle_window.draw(run_button.text);
 
-		battle_window.draw(this->pokemon.sprite);
-		battle_window.draw(pokemon.sprite);
-		battle_window.display();
+			battle_window.draw(this->pokemon.sprite);
+			battle_window.draw(pokemon.sprite);
+			battle_window.display();
 		}
 	}
 }
 
 void Battle::pokemonSelect()
 {
-	std::srand(std::time(nullptr));
-
 	switch (rand() % 4)
 	{
-	case 0:
+	case BULBASAUR:
 	{
-		pokemon.setPokemon(pokemon.BULBASAUR);
+		pokemon.setPokemon(BULBASAUR);
 		pokemon.texture.loadFromFile("Pokemon/Bulbasaur.png", sf::IntRect(0, 0, 128, 128));
 		pokemon.sprite.setTexture(this->pokemon.texture);
+		break;
 	}
-	case 1:
+
+	case CHARMANDER:
 	{
-		pokemon.setPokemon(pokemon.CHARMANDER);
+		pokemon.setPokemon(CHARMANDER);
 		pokemon.texture.loadFromFile("Pokemon/Charmander.png", sf::IntRect(0, 0, 128, 128));
 		pokemon.sprite.setTexture(this->pokemon.texture);
 		break;
 	}
-	case 2:
+
+	case SQUIRTLE:
 	{
-		pokemon.setPokemon(pokemon.SQUIRTLE);
+		pokemon.setPokemon(SQUIRTLE);
 		pokemon.texture.loadFromFile("Pokemon/Squirtle.png", sf::IntRect(0, 0, 128, 128));
 		pokemon.sprite.setTexture(this->pokemon.texture);
-
 		break;
 	}
-	case 3:
+
+	case PIKACHU:
 	{
-		pokemon.setPokemon(pokemon.PIKACHU);
+		pokemon.setPokemon(PIKACHU);
 		pokemon.texture.loadFromFile("Pokemon/Pikachu.png", sf::IntRect(0, 0, 128, 128));
 		pokemon.sprite.setTexture(this->pokemon.texture);
 		break;
 	}
 	}
 
-	pokemon.sprite.setPosition(325, 100);
+	pokemon.sprite.setPosition(300, 80);
 }
 
-void Battle::enemyAttack(sf::RenderWindow* battle_window, Pokemon* pokemon, sf::Text* player_pokemon_health, sf::Text* enemy_pokemon_health)
+void Battle::textInitialization(sf::Text* text, sf::Font* font, int character_size, std::string string, sf::Color color, float position_x, float position_y, bool set_origin)
+{
+	text->setFont(*font);
+	text->setCharacterSize(character_size);
+	text->setString(string);
+	text->setFillColor(color);
+
+	if (set_origin)
+	{
+		text->setOrigin(text->getLocalBounds().left + text->getLocalBounds().width, text->getLocalBounds().top + text->getLocalBounds().height);
+	}
+
+	text->setPosition(position_x, position_y);
+}
+
+void Battle::playerTurn(Button move1_button, Button move2_button, Button move3_button, Button move4_button, sf::Vector2i mouse_position)
+{
+	if (move1_button.isClicked(mouse_position))
+	{
+		//sf::Text battle_text;
+		//battle_text.setFont(font);
+		//battle_text.setCharacterSize(20);
+		//battle_text.setString(pokemon.getName() + " used " + pokemon.move1.name + ".");
+		//battle_text.setFillColor(sf::Color::Black);
+		//battle_text.setPosition(10.0f, 30.0f);
+
+		/*if (multiplier == 0.5)
+		{
+			battle_text.setString("It's not very effective.");
+		}
+		else if (multiplier == 2)
+		{
+			battle_text.setString("It's super effective!");
+		}*/
+
+		float multiplier = damageMultiplier(pokemon.move1.type, this->pokemon.type);
+		this->pokemon.hit_points -= pokemon.move1.value * multiplier;
+		enemy_pokemon_health.setString("Health: " + std::to_string(this->pokemon.hit_points));
+
+		if (this->pokemon.hit_points <= 0)
+		{
+			return;
+		}
+	}
+
+	if (move2_button.isClicked(mouse_position))
+	{
+		pokemon.hit_points += pokemon.move2.value;
+		player_pokemon_health.setString("Health: " + std::to_string(pokemon.hit_points));
+	}
+
+	if (move3_button.isClicked(mouse_position))
+	{
+		this->pokemon.hit_points -= pokemon.move3.value * damageMultiplier(pokemon.move3.type, this->pokemon.type);
+		enemy_pokemon_health.setString("Health: " + std::to_string(this->pokemon.hit_points));
+
+		if (this->pokemon.hit_points <= 0)
+		{
+			return;
+		}
+	}
+
+	if (move4_button.isClicked(mouse_position))
+	{
+		this->pokemon.hit_points -= pokemon.move4.value * damageMultiplier(pokemon.move4.type, this->pokemon.type);
+		enemy_pokemon_health.setString("Health: " + std::to_string(this->pokemon.hit_points));
+
+		if (this->pokemon.hit_points <= 0)
+		{
+			return;
+		}
+	}
+}
+
+void Battle::enemyTurn(sf::RenderWindow* battle_window, Pokemon* pokemon, sf::Text* player_pokemon_health, sf::Text* enemy_pokemon_health)
 {
 	std::srand(std::time(nullptr));
 
@@ -225,7 +241,7 @@ void Battle::enemyAttack(sf::RenderWindow* battle_window, Pokemon* pokemon, sf::
 	{
 	case 1:
 	{
-		pokemon->hit_points -= this->pokemon.move1.value;
+		pokemon->hit_points -= this->pokemon.move1.value * damageMultiplier(this->pokemon.move1.type, pokemon->type);
 		player_pokemon_health->setString("Health: " + std::to_string(pokemon->hit_points));
 
 		if (pokemon->hit_points <= 0)
@@ -243,7 +259,7 @@ void Battle::enemyAttack(sf::RenderWindow* battle_window, Pokemon* pokemon, sf::
 
 	case 3:
 	{
-		pokemon->hit_points -= this->pokemon.move3.value;
+		pokemon->hit_points -= this->pokemon.move3.value * damageMultiplier(this->pokemon.move3.type, pokemon->type);
 		player_pokemon_health->setString("Health: " + std::to_string(pokemon->hit_points));
 
 		if (pokemon->hit_points <= 0)
@@ -255,13 +271,184 @@ void Battle::enemyAttack(sf::RenderWindow* battle_window, Pokemon* pokemon, sf::
 
 	case 4:
 	{
-		pokemon->hit_points -= this->pokemon.move4.value;
+		pokemon->hit_points -= this->pokemon.move4.value * damageMultiplier(this->pokemon.move4.type, pokemon->type);
 		player_pokemon_health->setString("Health: " + std::to_string(pokemon->hit_points));
 
 		if (pokemon->hit_points <= 0)
 		{
 			battle_window->close();
 		}
+		break;
+	}
+	}
+}
+
+float Battle::damageMultiplier(int move_type, int pokemon_type)
+{
+	switch (move_type)
+	{
+	case NORMAL:
+	{
+		switch (pokemon_type)
+		{
+		case NORMAL:
+		{
+			return 1;
+		}
+
+		case GRASS:
+		{
+			return 1;
+		}
+
+		case FIRE:
+		{
+			return 1;
+		}
+
+		case WATER:
+		{
+			return 1;
+		}
+
+		case ELECTRIC:
+		{
+			return 1;
+		}
+		}
+
+		break;
+	}
+
+	case GRASS:
+	{
+		switch (pokemon_type)
+		{
+		case NORMAL:
+		{
+			return 1;
+		}
+
+		case GRASS:
+		{
+			return 0.5;
+		}
+
+		case FIRE:
+		{
+			return 0.5;
+		}
+
+		case WATER:
+		{
+			return 2;
+		}
+
+		case ELECTRIC:
+		{
+			return 1;
+		}
+		}
+
+		break;
+	}
+
+	case FIRE:
+	{
+		switch (pokemon_type)
+		{
+		case NORMAL:
+		{
+			return 1;
+		}
+
+		case GRASS:
+		{
+			return 2;
+		}
+
+		case FIRE:
+		{
+			return 0.5;
+		}
+
+		case WATER:
+		{
+			return 0.5;
+		}
+
+		case ELECTRIC:
+		{
+			return 1;
+		}
+		}
+
+		break;
+	}
+
+	case WATER:
+	{
+		switch (pokemon_type)
+		{
+		case NORMAL:
+		{
+			return 1;
+		}
+
+		case GRASS:
+		{
+			return 0.5;
+		}
+
+		case FIRE:
+		{
+			return 2;
+		}
+
+		case WATER:
+		{
+			return 0.5;
+		}
+
+		case ELECTRIC:
+		{
+			return 1;
+		}
+		}
+
+		break;
+	}
+
+	case ELECTRIC:
+	{
+		switch (pokemon_type)
+		{
+		case NORMAL:
+		{
+			return 1;
+		}
+
+		case GRASS:
+		{
+			return 0.5;
+		}
+
+		case FIRE:
+		{
+			return 1;
+		}
+
+		case WATER:
+		{
+			return 2;
+		}
+
+		case ELECTRIC:
+		{
+			return 0.5;
+		}
+		}
+
 		break;
 	}
 	}
--- NWP Seminarski Rad/Battle.h
+++ NWP Seminarski Rad/Battle.h
@@ -1,11 +1,13 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "Button.h"
 #include "Pokemon.h"
 
 class Battle 
 {
 	sf::Texture texture;
-
+	bool enemy_turn;
+	
 public:
 	Battle();
 	~Battle() {}
@@ -13,8 +15,15 @@ public:
 	Pokemon pokemon;
 	int chosen_pokemon;
 	sf::Sprite background;
+	sf::Text enemy_pokemon_name;
+	sf::Text enemy_pokemon_health;
+	sf::Text player_pokemon_name;
+	sf::Text player_pokemon_health;
 
 	void start(Pokemon pokemon);
 	void pokemonSelect();
-	void enemyAttack(sf::RenderWindow* battle_window, Pokemon* pokemon, sf::Text* player_pokemon_health, sf::Text* enemy_pokemon_health);
+	void playerTurn(Button move1, Button move2, Button move3, Button move4, sf::Vector2i mouse_position);
+	void enemyTurn(sf::RenderWindow* battle_window, Pokemon* pokemon, sf::Text* player_pokemon_health, sf::Text* enemy_pokemon_health);
+	float damageMultiplier(int move_type, int pokemon_type);
+	void textInitialization(sf::Text* text, sf::Font* font, int character_size, std::string string, sf::Color color, float position_x, float position_y, bool set_origin);
 };
